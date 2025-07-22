8a805e8d115deab16ae3090452b74cb7f44f9ebd
wrhz <1051280424@qq.com>
2025-07-22 18:01:39 +0800

    Update main.py
    For this update, we have addressed some unhandled bugs in the exception handling and, based on the previously cloned repository, added the functionality for initializing the repository.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from threading import Thread
 WIDTH: int = 400
 HEIGHT: int = 300
 
-options: list[str] = ["克隆仓库"]
+options: list[str] = ["初始化仓库", "克隆仓库"]
 option: str = ""
 
 with open("src/config.json", "r") as f:
@@ -19,10 +19,40 @@ if config.get("username"):
 if config.get("email"):
     subprocess.run(["git", "config", "--global", "user.email", config.get("email")])
 
+def do_init(init_path):
+    try:
+        repo = git.Repo.init(init_path)
+        print(repo)
+    except Exception as e:
+        error.config(text=str(e), fg="red")
+
 def do_clone(url, save_path):
     try:
         repo = git.Repo.clone_from(url, save_path)
         print(repo)
+    except Exception as e:
+        error.config(text=str(e), fg="red")
+
+def init_repo():
+    global option, tk, win, error
+
+    try:
+        if option == "init":
+            entries = [child for child in win.winfo_children() 
+                if isinstance(child, tk.Entry)]
+            if len(entries) >= 2:
+                input_init_path_entry = entries[1]
+                input_init_path = input_init_path_entry.get()
+                if input_init_path:
+                    Thread(
+                        target=do_init,
+                        args=(input_init_path,),
+                        daemon=True
+                    ).start()
+                else:
+                    error.config(text="请输入初始化仓库路径", fg="red")
+        else:
+            error.config(text="请选择相匹配的模式", fg="red")
     except Exception as e:
         error.config(text=str(e), fg="red")
 
@@ -53,6 +83,8 @@ def clone_repo():
                         ).start()
                 else:
                     error.config(text="请输入完整信息", fg="red")
+        else:
+            error.config(text="请选择相匹配的模式", fg="red")
     except Exception as e:
         error.config(text=str(e), fg="red")
 
@@ -67,6 +99,16 @@ def on_select_get_option(_):
                 widget.destroy()
         
         match now_option:
+            case "初始化仓库":
+                option = "init"
+                input_init_path_label = tk.Label(win, text="请输入初始化仓库路径")
+                input_init_path_label.pack()
+                input_init_path_entry = tk.Entry(win)
+                input_init_path_entry.pack()
+
+                init_button = tk.Button(win, text="初始化", command=init_repo)
+                init_button.pack()
+
             case "克隆仓库":
                 option = "clone"
                 input_url_or_name_label = tk.Label(win, text="请输入仓库地址或名称")
